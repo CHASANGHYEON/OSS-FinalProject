d23d26d9d0925751235b693843985523b174d0dc
CHASANGHYEON <82189042+CHASANGHYEON@users.noreply.github.com>
2022-05-21 13:24:06 +0900

    Upload a master file.
    Upload a master file.

---
+++ js/share.js
@@ -0,0 +1,34 @@
+const url = 'https://avengersmbtitest.netlify.app/';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '어벤져스 캐릭터로 알아보는 성격 유형 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ]
+  });
+}
+
